a876075ac0cc2544cb4157f8ff25b200617b64e0
Dmitry <Kononenko>
2020-10-31 23:12:37 +0300

    Начал разработку плеера

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Player from './components/Player'
 
 ReactDOM.render(
   <React.StrictMode>
      { /* Вставьте сюда ваш блок, который вы верстаете */ }
-     {/*<Header />*/}
+     <Player />
   </React.StrictMode>,
   document.getElementById('root')
 );
