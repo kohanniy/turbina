ebd31a30515c320db91bb00f5d6d0de9a42385f9
Dmitry <Kononenko>
2020-10-31 22:40:39 +0300

    Создана основная структура блока

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Main from './components/Main'
 
 ReactDOM.render(
   <React.StrictMode>
      { /* Вставьте сюда ваш блок, который вы верстаете */ }
-     {/*<Header />*/}
+     <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
