eedbcc769128ea751ab66e04fd92189fde5bfa1e
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-11-02 11:14:14 +0500

    добавить правильный текст в section, изменить название модификатора для секции с формой, добавить margin-bottom блоку songs, чтобы все было по макету

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section = function(props) {
   return (
-    <section className={`section section_${props.name}`}>
+    <section className={`section section_type_${props.name}`}>
       <h2 className="section__title">{props.title}</h2>
       {props.children}
     </section>
