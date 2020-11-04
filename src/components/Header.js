839464e2bf90a02b73dd90f7de2d4b5990e64c85
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-11-04 16:43:02 +0500

    привести в порядок CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,40 @@ import React from 'react';
 import logo from '../images/logo.svg';
 
 const Header = function() {
+  const [isLinksShow, setIsLinksShow] = React.useState(false);
+  const [windowWidth, setWindowWidth] = React.useState(0);
+  const [isButtonClick, setIsButtonClick] = React.useState(false);
+
+  const handleDropDownBtnClick = function() {
+    setIsLinksShow(!isLinksShow);
+    setIsButtonClick(!isButtonClick);
+  };
+
+  const resizeWindow = function() {
+    setWindowWidth(window.innerWidth);
+  };
+
+  React.useEffect(() => {
+    resizeWindow();
+    window.addEventListener('resize', resizeWindow);
+    return () => window.removeEventListener('resize', resizeWindow);
+  }, []);
+
   return (
     <div className="header">
       <a href="https://localhost:3000" className="header__logo-link">
         <img src={logo} alt="Логотип" className="header__logo"/>
       </a>
-      <div className="header__links">
-        <a href="https://localhost:3000" className="header__link">Яндекс.Музыка ↗</a>
-        <a href="https://localhost:3000" className="header__link">Spotify ↗</a>
-        <a href="https://localhost:3000" className="header__link">Apple Music ↗</a>
-        <a href="https://localhost:3000" className="header__link">VK Music ↗</a>
+      <div className="header__dropdown">
+        <button className={`header__dropdown-btn ${isButtonClick ? 'header__dropdown-btn_hide-links' : 'header__dropdown-btn_show-links'}`} onClick={handleDropDownBtnClick}>
+          <span>Стриминги</span>
+        </button>
+        <div className={`header__links ${(windowWidth > 426 || isLinksShow) ? '' : 'hide-block'}`} >
+          <a href="https://localhost:3000" className="header__link">Яндекс.Музыка ↗</a>
+          <a href="https://localhost:3000" className="header__link">Spotify ↗</a>
+          <a href="https://localhost:3000" className="header__link">Apple Music ↗</a>
+          <a href="https://localhost:3000" className="header__link">VK Music ↗</a>
+        </div>
       </div>
     </div>
   );
