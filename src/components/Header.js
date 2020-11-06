915f5f401bb00f06ab13e020fb4a5b5021243617
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-11-06 16:10:16 +0500

    достать кнопки при высоком разрешении

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,16 @@ import logo from '../images/logo.svg';
 import { CSSTransition } from 'react-transition-group'
 
 const Header = function() {
-  const [isLinksShow, setIsLinksShow] = React.useState(false);
   const [windowWidth, setWindowWidth] = React.useState(0);
   const [isButtonClick, setIsButtonClick] = React.useState(false);
 
   const handleDropDownBtnClick = function() {
-    setIsLinksShow(!isLinksShow);
     setIsButtonClick(!isButtonClick);
   };
 
   const resizeWindow = function() {
     setWindowWidth(window.innerWidth);
-    if (windowWidth > 426 && isLinksShow) {
-      setIsLinksShow(!isLinksShow);
+    if (windowWidth > 426 && isButtonClick) {
       setIsButtonClick(!isButtonClick);
     }
   };
@@ -32,19 +29,29 @@ const Header = function() {
       <a href="https://localhost:3000" className="header__logo-link">
         <img src={logo} alt="Логотип" className="header__logo"/>
       </a>
-      <div className="header__dropdown">
-        <button className={`header__dropdown-btn ${isButtonClick ? 'header__dropdown-btn_hide-links' : 'header__dropdown-btn_show-links'}`} onClick={handleDropDownBtnClick}>
-          <span>Стриминги</span>
-        </button>
-        <CSSTransition in={isButtonClick} timeout={400} classNames="header__links-animation" unmountOnExit={true} mountOnEnter={true}>
+      {
+        windowWidth > 426 ?
           <div className={`header__links`} >
             <a href="https://localhost:3000" className="header__link">Яндекс.Музыка ↗</a>
             <a href="https://localhost:3000" className="header__link">Spotify ↗</a>
             <a href="https://localhost:3000" className="header__link">Apple Music ↗</a>
             <a href="https://localhost:3000" className="header__link">VK Music ↗</a>
           </div>
-        </CSSTransition>
-      </div>
+        :
+          <div className="header__dropdown">
+            <button className={`header__dropdown-btn ${isButtonClick ? 'header__dropdown-btn_hide-links' : 'header__dropdown-btn_show-links'}`} onClick={handleDropDownBtnClick}>
+              <span>Стриминги</span>
+            </button>
+            <CSSTransition in={isButtonClick} timeout={400} classNames="header__links-animation" unmountOnExit={true} mountOnEnter={true}>
+              <div className={`header__links header__links_mobile`} >
+                <a href="https://localhost:3000" className="header__link">Яндекс.Музыка ↗</a>
+                <a href="https://localhost:3000" className="header__link">Spotify ↗</a>
+                <a href="https://localhost:3000" className="header__link">Apple Music ↗</a>
+                <a href="https://localhost:3000" className="header__link">VK Music ↗</a>
+              </div>
+            </CSSTransition>
+          </div>
+      }
     </div>
   );
 }
